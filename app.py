52ec2a1e32506c1a7065b4d46a7379a470d87561
Ali Raza <ali.raza@ssasoft.com>
2023-08-14 17:36:28 +0500

    create end points

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask
+from flask_restful import Api
+
 from todo_app.data_access.sql_alchemy import register_sqlalchemy
+from todo_app.resources import TodoResource, TodoCreateResource
 from todo_app.env_configs.development import Local
+
+
 
 
 def create_app(config_object):
     flask_app = Flask(__name__)
-
+    api = Api(flask_app)
+    
     flask_app.config.from_object(config_object)
     initialize_sqlalchemy(flask_app)
+    api.add_resource(TodoResource, '/todo/<int:id>')
+    api.add_resource(TodoCreateResource, '/todo/')
     return flask_app
 
 
